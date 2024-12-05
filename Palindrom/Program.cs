90f11d91d799f38bd93be79d40d141452ad5dc2e
HristoHackera123 <hrisi.belezirev@gmail.com>
2024-12-05 22:54:56 +0200

    Domashna

---
+++ Palindrom/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+namespace Palindrom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input to check palindrom: ");
+            string input = Console.ReadLine();
+            StackWay(input);
+            ReverseWay(input);
+        }
+        static void StackWay(string input)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            Stack<char> stack = new Stack<char>(input);
+            foreach (char item in input)
+            {
+                if (item == stack.Peek())
+                {
+                    stack.Pop();
+                }
+            }
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("It is palindrom");
+            }
+            else
+            {
+                Console.WriteLine("It is not palindrom");
+            }
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedTicks);
+        }
+        static void ReverseWay(string input)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            string reversed = input;
+            reversed.Reverse();
+            string result = string.Compare(input, reversed) == 0 ? "Palindrom" : "Not Palindrom";
+            Console.WriteLine(result);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedTicks);
+        }
+    }
+}
---
+++ Zad/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Zad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Put a combination of brackets: ");
+            string input = Console.ReadLine();
+            CheckBrackets(input);
+        }
+        static void CheckBrackets(string input)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in input)
+            {
+                switch (c)
+                {
+                    case '(':
+                        stack.Push(c);
+                        break;
+                    case ')':
+                        if (stack.Count == 0)
+                        {
+                            Console.WriteLine("The brackets arent balanced(1)");
+                            return;
+                        }
+                        stack.Pop();
+                        break;
+                }
+            }
+
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("The brackets are all good");
+            }
+            else
+            {
+                Console.WriteLine("Brackets arent balanced");
+            }
+        }
+    }
+}
